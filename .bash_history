1e19e38287d7f3298b041c8debe0126c6930c3b8
Ubuntu <ubuntu@ip-172-31-39-56.ec2.internal>
2023-11-07 21:51:20 +0000

    SOV

---
+++ .bash_history
@@ -0,0 +1,21 @@
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt mysql-server
+sudo apt install  mysql-server
+sudo su
+sudo mysql -u root
+sudo mysql -u root -p
+mysql -u root -p
+
+sudo apt install npm
+nano app.js
+ls
+npm init
+ls
+nano package.json
+node app.js
+nano app.js
+npm i mysql
+node app.js
+nano app.js
+sudo apt install phpmyadmin
---
+++ app.js
@@ -0,0 +1,38 @@
+var mysql = require('mysql');
+
+var conexion = mysql.createConnection({
+   host:'localhost',
+   database:'sakila',
+   user:'aChavez',
+   password:'12345678'
+})
+
+conexion.connect(function(error){
+   if(error){
+      throw error;
+   }else{
+      console.log('CONEXION EXITOSA');  
+   }
+});
+
+conexion.query('SELECT * FROM actor',function(error,results,fields){
+  if(error)
+    throw error;
+
+ results.forEach(result =>{
+console.log(result);
+});
+})
+
+//Insertar datos a mysql
+ conexion.query('INSERT INTO actor (first_name , last_name)VALUES("Juan","Apasa")',function (error,results){
+  if(error) throw error;
+      console.log("Registro Agregado",results)  
+});
+
+//actuaizar los datos
+/*conexion.query('UPDATE usuarios SET first_name = "Steve", last_name="Rogers" WHERE actor_id=199' , function(error, result){
+if(error) thron error;
+console.log('!Registro actualisado¡, result);
+});*/	
+conexion.end();
